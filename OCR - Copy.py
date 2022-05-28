b320fdee7c6cbda5ba035053b93227364e72c16c
ShiratsuYudachi <shiratsuyudachi@gmail.com>
2022-05-29 03:02:11 +0800

    Support MultiProcess OCR

--- OCR - Copy.py
+++ OCR - Copy.py
@@ -2,36 +2,59 @@ from utilities import sortList
 import easyocr
 import os
 import multiprocessing as mp
+import time
+
 path = path = os.path.dirname(__file__)+'\\'
 subtitleImagePath = path+'SubtitleImages\\'
 def read(imagePath):
     reader = easyocr.Reader(['ch_sim','en'])
     result = reader.readtext(imagePath, detail=0)
     rawText = '，'.join(result)
-    text = [rawText,imagePath.split("\\")[-2],imagePath.split("\\")[-1].strip('.jpg')]
+    text = [rawText,imagePath.split("\\")[-2],imagePath.split("\\")[-1].strip('.jpg')]#format:[text, video, frame]
     print(text)
 
-def readMP(imagePath,q):
+def readMP(imagePath):
     reader = easyocr.Reader(['ch_sim','en'])
     result = reader.readtext(imagePath, detail=0)
     rawText = '，'.join(result)
     text = [rawText,imagePath.split("\\")[-2],imagePath.split("\\")[-1].strip('.jpg')]
-    print(text)
-    q.put(1)
+    print("extracted "+str(text))
+    return text
 
 def readall():
+    todo = []
     for i in sortList(os.listdir(subtitleImagePath)):
         print(os.listdir(subtitleImagePath))
         if os.path.isdir(subtitleImagePath+i):
             for k in sortList(os.listdir(subtitleImagePath+i)):
-                read(subtitleImagePath+i+'\\'+k)
-                print('processed '+subtitleImagePath+i+'\\'+k)
+                todo.append(subtitleImagePath+i+'\\'+k)
+                print('added '+subtitleImagePath+i+'\\'+k)
         else:
-            read(subtitleImagePath+i)
-            print('processed '+ subtitleImagePath+i)
+            todo.append(subtitleImagePath+i)
+            print('added '+ subtitleImagePath+i)
 
 def readallMP():
-    pass
-
+    #pool.apply_async(readMP, args=(r"D:\python\KichikuHelper\SubtitleImages\example.mp4\20.jpg", q))
+    #pool.close()
+    #pool.join()
+    todo = []
+    for i in sortList(os.listdir(subtitleImagePath)):
+        print(os.listdir(subtitleImagePath))
+        if os.path.isdir(subtitleImagePath+i):
+            for k in sortList(os.listdir(subtitleImagePath+i)):
+                todo.append(subtitleImagePath+i+'\\'+k)
+                print('added '+subtitleImagePath+i+'\\'+k)
+        else:
+            todo.append(subtitleImagePath+i)
+            print('added '+ subtitleImagePath+i)
+    print(todo)
+    q = mp.Queue()
+    pool = mp.Pool(6)
+    res = pool.map(readMP,todo)
+    print(res)
 if __name__ == "__main__":  
-    readall()
+    #readall()
+    start_time = time.time()
+    readallMP()
+    end_time = time.time()
+    print('Time elapsed: '+str(end_time-start_time))
